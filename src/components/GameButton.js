dfc3d2f474c416f7896702eaebc39067899da8cf
JIJU-GFT <jiju@gft.com>
2022-03-18 18:04:28 +0100

    Styles can now be provided to the button component from the parent

--- src/components/GameButton.js
+++ src/components/GameButton.js
@@ -1,15 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const GameButton = ({title, onClick}) => {
+const GameButton = ({title, onClick, buttonType}) => {
     return (
-        <button onClick={onClick} className="App-button">{title}</button>
+        <button onClick={onClick} className={buttonType}>{title}</button>
     );
 };
 
 GameButton.propTypes = {
     title: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
+    buttonType: PropTypes.string.isRequired
 };
 
 export default GameButton;
