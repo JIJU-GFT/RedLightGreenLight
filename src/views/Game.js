ad40869dab6f20382f47c19e7e885b5255ebf8b8
JIJU-GFT <jiju@gft.com>
2022-03-18 19:11:43 +0100

    Game screen now controls if the user double steps with the same foot

--- src/views/Game.js
+++ src/views/Game.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import redLight from "@images/redlight.png";
 import greenLight from "@images/greenlight.png";
 import GameButton from "@components/GameButton.js";
-import PropTypes from "prop-types";
 
 function Game() {
+  // Loading the navigation parameters
   let { state } = useLocation();
 
-  function handleClick() {}
+  const [lastClicked, setLastClicked] = useState("none");
+  const [score, setScore] = useState();
+
+
+  // Will reset last step if the user loses the points so they can start anew
+  function handleClick(buttonPressed) {
+    let step = buttonPressed == 0 ? "Left" : "Right";
+
+    if (lastClicked === "none") {
+      setLastClicked(step);
+      setScore(1);
+    } else if (lastClicked.localeCompare(step) === 0) {
+      setLastClicked("none");
+      setScore(0);
+    } else {
+      setLastClicked(step);
+      setScore((prevScore) => prevScore + 1);
+    }
+  }
 
   return (
     <div className="Home">
@@ -16,25 +34,23 @@ function Game() {
         <img src={redLight} className="App-logo" alt="logo" />
         <img src={greenLight} className="App-logo" alt="logo" />
         <h3>This is the game screen, {state.username}.</h3>
+        <h3>Score: {score}.</h3>
+        <h3>Last clicked: {lastClicked}.</h3>
       </header>
       <div className="Game-body">
         <GameButton
           title="Left"
           buttonType="Game-button"
-          onClick={handleClick}
+          onClick={() => handleClick(0)}
         />
         <GameButton
           title="Right"
           buttonType="Game-button"
-          onClick={handleClick}
+          onClick={() => handleClick(1)}
         />
       </div>
     </div>
   );
 }
-
-Game.propTypes = {
-  username: PropTypes.string,
-};
 
 export default Game;
