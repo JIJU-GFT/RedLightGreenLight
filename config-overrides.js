75601cb731bed1b768b87fd9224cc1450b89d15f
JIJU-GFT <jiju@gft.com>
2022-03-18 14:39:34 +0100

    Added component path alias

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,8 @@ const { alias } = require('react-app-rewire-alias');
 module.exports = function override(config) {
   alias({
     '@images': 'src/assets/images',
-    '@views': 'src/views/'
+    '@views': 'src/views/',
+    '@components': 'src/components'
   })(config);
 
   return config;
