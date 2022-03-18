b866f50fea154e2cef431850d098d84896cf4b8d
JIJU-GFT <jiju@gft.com>
2022-03-18 18:04:07 +0100

    Created game screen route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Home from "@views/Home/Home.js";
+import Home from "@views/Home.js";
+import Game from "@views/Game.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/RedLightGreenLight" element={<Home/>}>
+        </Route>
+        <Route exact path="/Game" element={<Game/>}>
         </Route>
       </Routes>
     </Router>
