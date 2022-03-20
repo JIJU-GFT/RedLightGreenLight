0b8d635d56dc2bcca416275274d97ba924b1c036
JIJU-GFT <jiju@gft.com>
2022-03-20 10:59:02 +0100

    Created a text display component

---
+++ src/components/GameText.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GameText = ({text, textStyles}) => {
+    return (
+        <p className={textStyles ? textStyles : "defaultText"}>{text}</p>
+    );
+};
+
+GameText.propTypes = {
+    text: PropTypes.string.isRequired,
+    textStyles: PropTypes.string
+};
+
+export default GameText;
