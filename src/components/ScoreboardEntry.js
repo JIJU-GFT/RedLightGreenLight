a0bfeeef0886e6542132d4449dbd4fc6bad9d728
JIJU-GFT <jiju@gft.com>
2022-03-21 16:59:50 +0100

    Tweaked scoreboard entry component

--- src/components/ScoreboardEntry.js
+++ src/components/ScoreboardEntry.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 const ScoreboardEntry = ({ scoreEntry }) => {
   return (
-    <div className="Score-entry">
-      <p>{scoreEntry.username}</p>
-      <p>{scoreEntry.highScore}</p>
-    </div>
+      <div className="Score-entry">
+        <span className="user">{scoreEntry.username}</span>
+        <span className="score">{scoreEntry.highScore} pts.</span>
+      </div>
   );
 };
 
