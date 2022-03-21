480db9d6ba845cd7654b70e419fb9bc19436e353
JIJU-GFT <jiju@gft.com>
2022-03-21 12:10:10 +0100

    Added scoreboard route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Home from "@views/Home.js";
 import Game from "@views/Game.js";
+import Scoreboard from "@views/Scoreboard.js";
+
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/Home" element={<Home />} />
         <Route exact path="/Game" element={<Game />} />
+        <Route exact path="/Scoreboard" element={<Scoreboard />} />
         <Route path="*" element={<Navigate to="/Home"/>} replace />
       </Routes>
     </Router>
